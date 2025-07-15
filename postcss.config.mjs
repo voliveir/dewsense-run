d8b4f0736b3bd32b37e4c2198d815a63f34bc2ae
Vanessa Oliveira <oliveirav09@gmail.com>
2025-07-15 16:58:16 -0400

    Initial commit v2

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,10 +1,4 @@
-const config = {
-  plugins: ["@tailwindcss/postcss"],
-};
-
-export default config;
-
-module.exports = {
+export default {
   plugins: {
     tailwindcss: { config: './tailwind.config.js' },
     autoprefixer: {},
